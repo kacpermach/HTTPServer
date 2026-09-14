6a5bb6421a951c0fcc38b01a1d5ecfee1f1e9b28
Kacper Mach <kacpermach03@gmail.com>
2026-09-14 17:29:53 +0200

    feat(HTTPServer): Modify code to work also on Windows.

--- src/HTTPServer.c
+++ src/HTTPServer.c
@@ -1,34 +1,63 @@
-#include "HTTPServer.h"
+#include "HTTPServer.h" // Pamiętaj, aby plik nagłówkowy też był platform-agnostic
 
-int main(){
+int main() {
+    // 1. Inicjalizacja Winsock (tylko na Windows)
+#ifdef _WIN32
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        printf("WSAStartup error\n");
+        return 1;
+    }
+#endif
+
+    // Używamy typu SOCKET (na Linuxie to zwykły int)
+    SOCKET server_socket_fd, client_socket_fd;
+    struct sockaddr_in server_address, client_address;
+    socklen_t client_address_len;
+    int socket_opt = 1;
+
     server_socket_fd = socket(AF_INET, SOCK_STREAM, 0);
-
-    if (server_socket_fd < 0){
-        perror("Socket initialization error\n");
+    if (server_socket_fd == INVALID_SOCKET){
+        perror("Socket initialization error");
     }
 
-    if(setsockopt(server_socket_fd, SOL_SOCKET, SO_REUSEADDR, &socket_opt, sizeof(socket_opt)) < 0){
-        perror("Socket option initialization error\n");
+    // Rzutowanie (const char*) jest wymagane przez Windows API, na Linuxie zadziała bez problemu
+    if(setsockopt(server_socket_fd, SOL_SOCKET, SO_REUSEADDR, (const char*)&socket_opt, sizeof(socket_opt)) < 0){
+        perror("Socket option initialization error");
     }
 
     server_address.sin_family = AF_INET;
     server_address.sin_addr.s_addr = INADDR_ANY;
     server_address.sin_port = htons(PORT);
 
-    if(bind(server_socket_fd, (struct sockaddr*)&server_address, sizeof(server_address))<0){
-        perror("Binding error\n");
+    if(bind(server_socket_fd, (struct sockaddr*)&server_address, sizeof(server_address)) < 0){
+        perror("Binding error");
     }
 
     if(listen(server_socket_fd, LISTEN_BACKLOG) < 0){
-        perror("Listen error\n");
+        perror("Listen error");
     }
+    
     client_address_len = sizeof(client_address);
     client_socket_fd = accept(server_socket_fd, (struct sockaddr*)&client_address, &client_address_len);
 
-    if(client_socket_fd < 0){
-        perror("Client socket error\n");
+    if(client_socket_fd == INVALID_SOCKET){
+        perror("Client socket error");
     }
-    while(client_socket_fd){
-        printf("%d\n", client_socket_fd);
+    
+    // Przykładowa pętla, uważaj, żeby nie zablokowała konsoli
+    if(client_socket_fd != INVALID_SOCKET){
+        printf("Klient podłączony! FD: %d\n", (int)client_socket_fd);
     }
+
+    // Zamykanie gniazd (makro dopasowane do systemu)
+    CLOSE_SOCKET(client_socket_fd);
+    CLOSE_SOCKET(server_socket_fd);
+
+    // Czyszczenie Winsock (tylko na Windows)
+#ifdef _WIN32
+    WSACleanup();
+#endif
+
+    return 0;
 }
--- src/HTTPServer.h
+++ src/HTTPServer.h
@@ -1,13 +1,22 @@
-#ifdef __APPLE__
+#ifdef _WIN32
     #include <stdio.h>
+    #include <winsock2.h>
+    #include <ws2tcpip.h>
+
+    typedef int socklen_t;
+    #define CLOSE_SOCKET closesocket
+#else
+    #include <stdio.h>
+    #include <stdlib.h>
+    #include <unistd.h>
     #include <sys/socket.h>
     #include <netinet/in.h>
+
+    #define SOCKET int
+    #define INVALID_SOCKET -1
+    #define SOCKET_ERROR -1
+    #define CLOSE_SOCKET close
 #endif
 
 #define PORT 8080
 #define LISTEN_BACKLOG 50
-
-int server_socket_fd, client_socket_fd;
-struct sockaddr_in server_address, client_address;
-socklen_t client_address_len;
-int socket_opt = 1;
